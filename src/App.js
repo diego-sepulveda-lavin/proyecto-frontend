95721809334dfd1843e9b728eb838c9ed3b42e38
Diego <diegosl@hotmail.cl>
2020-07-08 14:59:22 -0400

    Primer commit projecto frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 
-function App() {
+import injectContext from './store/appContext';
+
+import Dashboard from './views/Dashboard'
+import LandingPage from './views/LandingPage'
+
+
+const App = props => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={LandingPage}></Route>
+        <Route path='/dashboard' exact component={Dashboard}></Route>
+
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default injectContext(App);
