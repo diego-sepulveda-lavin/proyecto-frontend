b330b96cfb3f06e85997784def21108b6e4cbb4c
Diego <diegosl@hotmail.cl>
2020-07-09 10:46:40 -0400

    Fix script fecha templates

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -224,10 +224,7 @@ const LandingPage = () => {
                                 </ul>
                             </nav>
                             <div className="copyright" id="copyright">
-                                &copy;
-                                <script>
-                                                            document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-                                </script>, Designed by
+                                &copy; {new Date().getFullYear()} Designed by
                                 <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by
                                 <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
                             </div>
