0b61d3eea768e06b2d40490f252d042a26b2d27a
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2020-07-09 15:38:19 -0400

    vistas ventaPrincipal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import injectContext from './store/appContext';
 
 import Dashboard from './views/Dashboard'
 import LandingPage from './views/LandingPage'
+import VentaUsuario from './views/ventaPrincipal';
 
 
 const App = props => {
@@ -12,7 +13,8 @@ const App = props => {
     <BrowserRouter>
       <Switch>
         <Route path='/' exact component={LandingPage}></Route>
-        <Route path='/dashboard' exact component={Dashboard}></Route>
+        <Route path='/dashboard' component={Dashboard}></Route>
+        <Route exact path="/ventaUsuario" component={VentaUsuario}></Route>
 
       </Switch>
     </BrowserRouter>
---
+++ src/components/footer.js
@@ -0,0 +1,36 @@
+import React from "react"
+
+const Footer = props =>{
+    return (
+        <>
+        <footer className="footer">
+                        <div className=" container-fluid ">
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <a href="https://www.creative-tim.com">
+                                            Creative Tim
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="http://presentation.creative-tim.com">
+                                            About Us
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="http://blog.creative-tim.com">
+                                            Blog
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                            <div className="copyright" id="copyright">
+                                &copy; {new Date().getFullYear()} Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+                            </div>
+                        </div>
+                    </footer>
+        </>
+    )
+}
+
+export default Footer;
