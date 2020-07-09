b5de147ff51bfd8ecf77b7308d42004c47fba800
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2020-07-09 17:11:04 -0400

    Implementacion de menu lateral y redirecciones

--- src/components/menuLateral.js
+++ src/components/menuLateral.js
@@ -1,70 +1,74 @@
 import React from "react"
+import { Link } from "react-router-dom";
 
-const MenuLateral = props =>{
+const MenuLateral = props => {
     return (
         <div className="sidebar" data-color="orange">
-        {/* <!--Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"--> */}
-        <div className="logo">
-            <a href="http://www.creative-tim.com" className="simple-text logo-mini">
-                CT
+            {/* <!--Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"--> */}
+            <div className="logo">
+                <a href="http://www.creative-tim.com" className="simple-text logo-mini">
+                    CT
             </a>
-            <a href="http://www.creative-tim.com" className="simple-text logo-normal">
-                Creative Tim
+                <a href="http://www.creative-tim.com" className="simple-text logo-normal">
+                    DHSL
             </a>
+            </div>
+            <div className="sidebar-wrapper" id="sidebar-wrapper">
+                <ul className="nav">
+                    <li className="active ">
+                        <Link to={"/dashboard"}>
+                            <i className="now-ui-icons design_app"></i>
+                            <p>Dashboard</p>
+                        </Link>
+                    </li>
+                    <li className="active-pro">
+                        <Link>
+                            <i className="now-ui-icons text_caps-small"></i>
+                            <p data-toggle="collapse" data-target="#ventas">Ventas</p>
+                        </Link>
+
+                        <div id="ventas" class="collapse">                            
+                            <li className="ml-5"><Link to={"/ventaUsuario"}><p>Ingresar Ventas</p></Link></li>
+                            <li className="ml-5"><Link><p>Abrir Caja</p></Link></li>
+                            <li className="ml-5"><Link><p>Cierre de Caja</p></Link></li>
+                            <li className="ml-5"><Link><p>Anular Venta</p></Link></li>
+                            <li className="ml-5"><Link><p>Cuadrar Caja</p></Link></li>                            
+                        </div>
+                    </li>
+                    <li>
+                        <Link>
+                            <i className="now-ui-icons location_map-big"></i>
+                            <p data-toggle="collapse" data-target="#stock">Stock</p>
+                        </Link>
+                        <div id="stock" class="collapse">                            
+                            <li className="ml-5"><Link to={"/general-stock"}><p>Ver Inventario</p></Link></li>
+                            <li className="ml-5"><Link to={"/crear-sku"}><p>Crear SKU</p></Link></li>
+                            <li className="ml-5"><Link to={"/modificar-sku"}><p>Modificar SKU</p></Link></li>
+                            <li className="ml-5"><Link to={"/ingresar-factura"}><p>Ingresar Factura</p></Link></li>                          
+                        </div>
+                    </li>
+                    <li>
+                        <Link>
+                            <i className="now-ui-icons ui-1_bell-53"></i>
+                            <p data-toggle="collapse" data-target="#administrador">Administrador</p>
+                        </Link>
+                        
+                        <div id="administrador" class="collapse">                            
+                            <li className="ml-5"><Link to={""}><p>Ver Usuarios</p></Link></li>
+                            <li className="ml-5"><Link><p>Crear Usuarios</p></Link></li>                     
+                        </div>
+                    </li>
+                   
+                   {/*  <li>
+                        <a href="./typography.html">
+                            <i className="now-ui-icons text_caps-small"></i>
+                            <p>Typography</p>
+                        </a>
+                    </li> */}
+                  
+                </ul>
+            </div>
         </div>
-        <div className="sidebar-wrapper" id="sidebar-wrapper">
-            <ul className="nav">
-                <li className="active ">
-                    <a href="./dashboard.html">
-                        <i className="now-ui-icons design_app"></i>
-                        <p>Dashboard</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./icons.html">
-                        <i className="now-ui-icons education_atom"></i>
-                        <p>Icons</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./map.html">
-                        <i className="now-ui-icons location_map-big"></i>
-                        <p>Maps</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./notifications.html">
-                        <i className="now-ui-icons ui-1_bell-53"></i>
-                        <p>Notifications</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./user.html">
-                        <i className="now-ui-icons users_single-02"></i>
-                        <p>User Profile</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./tables.html">
-                        <i className="now-ui-icons design_bullet-list-67"></i>
-                        <p>Table List</p>
-                    </a>
-                </li>
-                <li>
-                    <a href="./typography.html">
-                        <i className="now-ui-icons text_caps-small"></i>
-                        <p>Typography</p>
-                    </a>
-                </li>
-                <li className="active-pro">
-                    <a href="./upgrade.html">
-                        <i className="now-ui-icons arrows-1_cloud-download-93"></i>
-                        <p>Upgrade to PRO</p>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
     )
 }
 
--- src/views/CrearSKU.js
+++ src/views/CrearSKU.js
@@ -1,141 +1,21 @@
 import React from 'react';
 import '../css/now-ui-dashboard.css'
+import Footer from '../components/footer';
+import MenuLateral from '../components/menuLateral';
+import NavBar from '../components/navBar';
 
 const CrearSku = () => {
     return (
         <>
             <div className="wrapper ">
-                <div className="sidebar" data-color="orange">
-                    {/* <!--
-        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
-    --> */}
-                    <div className="logo">
-                        <a href="http://www.creative-tim.com" className="simple-text logo-mini">
-                            CT
-        </a>
-                        <a href="http://www.creative-tim.com" className="simple-text logo-normal">
-                            Creative Tim
-        </a>
-                    </div>
-                    <div className="sidebar-wrapper" id="sidebar-wrapper">
-                        <ul className="nav">
-                            <li>
-                                <a href="./dashboard.html">
-                                    <i className="now-ui-icons design_app"></i>
-                                    <p>Dashboard</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="./icons.html">
-                                    <i className="now-ui-icons education_atom"></i>
-                                    <p>Icons</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="./map.html">
-                                    <i className="now-ui-icons location_map-big"></i>
-                                    <p>Maps</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="./notifications.html">
-                                    <i className="now-ui-icons ui-1_bell-53"></i>
-                                    <p>Notifications</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="./user.html">
-                                    <i className="now-ui-icons users_single-02"></i>
-                                    <p>User Profile</p>
-                                </a>
-                            </li>
-                            <li className="active ">
-                                <a href="./tables.html">
-                                    <i className="now-ui-icons design_bullet-list-67"></i>
-                                    <p>Table List</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="./typography.html">
-                                    <i className="now-ui-icons text_caps-small"></i>
-                                    <p>Typography</p>
-                                </a>
-                            </li>
-                            <li className="active-pro">
-                                <a href="./upgrade.html">
-                                    <i className="now-ui-icons arrows-1_cloud-download-93"></i>
-                                    <p>Upgrade to PRO</p>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div className="main-panel" id="main-panel">
+            <MenuLateral />
+            <div className="main-panel" id="main-panel">
                     {/* <!-- Navbar --> */}
-                    <nav className="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute">
-                        <div className="container-fluid">
-                            <div className="navbar-wrapper">
-                                <div className="navbar-toggle">
-                                    <button type="button" className="navbar-toggler">
-                                        <span className="navbar-toggler-bar bar1"></span>
-                                        <span className="navbar-toggler-bar bar2"></span>
-                                        <span className="navbar-toggler-bar bar3"></span>
-                                    </button>
-                                </div>
-                                <a className="navbar-brand" href="#pablo">Table List</a>
-                            </div>
-                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-                                <span className="navbar-toggler-bar navbar-kebab"></span>
-                                <span className="navbar-toggler-bar navbar-kebab"></span>
-                                <span className="navbar-toggler-bar navbar-kebab"></span>
-                            </button>
-                            <div className="collapse navbar-collapse justify-content-end" id="navigation">
-                                <form>
-                                    <div className="input-group no-border">
-                                        <input type="text" value="" className="form-control" placeholder="Search..." />
-                                        <div className="input-group-append">
-                                            <div className="input-group-text">
-                                                <i className="now-ui-icons ui-1_zoom-bold"></i>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                                <ul className="navbar-nav">
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#pablo">
-                                            <i className="now-ui-icons media-2_sound-wave"></i>
-                                            <p>
-                                                <span className="d-lg-none d-md-block">Stats</span>
-                                            </p>
-                                        </a>
-                                    </li>
-                                    <li className="nav-item dropdown">
-                                        <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                            <i className="now-ui-icons location_world"></i>
-                                            <p>
-                                                <span className="d-lg-none d-md-block">Some Actions</span>
-                                            </p>
-                                        </a>
-                                        <div className="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                                            <a className="dropdown-item" href="#">Action</a>
-                                            <a className="dropdown-item" href="#">Another action</a>
-                                            <a className="dropdown-item" href="#">Something else here</a>
-                                        </div>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a className="nav-link" href="#pablo">
-                                            <i className="now-ui-icons users_single-02"></i>
-                                            <p>
-                                                <span className="d-lg-none d-md-block">Account</span>
-                                            </p>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
+                    <NavBar />
                     {/* <!-- End Navbar --> */}
-                    <div className="panel-header panel-header-sm">
+                    <div className="panel-header panel-header-md">
+                        <h1 className="text-warning text-center">Stock</h1>
+                        <h3 className="text-info text-center">Crear SKU</h3>
                     </div>
                     <div className="content mt-2">
                         <div className="row">
@@ -224,34 +104,7 @@ const CrearSku = () => {
                             </div>
                         </div>
                     </div>
-                    <footer className="footer">
-                        <div className=" container-fluid ">
-                            <nav>
-                                <ul>
-                                    <li>
-                                        <a href="https://www.creative-tim.com">
-                                            Creative Tim
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="http://presentation.creative-tim.com">
-                                            About Us
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="http://blog.creative-tim.com">
-                                            Blog
-                                        </a>
-                                    </li>
-                                </ul>
-                            </nav>
-                            <div className="copyright" id="copyright">
-                                &copy; <script>
-                                    document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
-            </script>, Designed by <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
-          </div>
-                        </div>
-                    </footer>
+                    <Footer/>
                 </div>
             </div>
 
