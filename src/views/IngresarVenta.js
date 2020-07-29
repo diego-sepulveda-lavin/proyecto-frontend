9cd34921956a2f47fefd8c5ddfce97f06b86a45d
Humberto Lagomarsino G <h.lagomarsino.g@gmail.com>
2020-07-29 17:39:41 -0400

    cambio en el muestreo de informacion con el iva, total neto y total compra

--- src/views/IngresarVenta.js
+++ src/views/IngresarVenta.js
@@ -28,13 +28,30 @@ const IngresarVenta = (props) => {
         buscador: "",
         detalleProductos: [],
         datosVenta: {
-            monto_total : 0,
-            monto_neto : 0,
-            monto_iva : 0
+            total_neto: null,
+            iva: null,
+            total_a_pagar: null
         }
     })
 
-    const getDatosVentas = ()=>{}
+    const getDatosVentas = (e) => {
+        const { datosVenta } = state
+
+
+        datosVenta["total_neto"] = state.datosVenta.monto_total / (1.19).toFixed(2);
+        datosVenta["iva"] = state.datosVenta.monto_neto * (19 / 100).toFixed(2);
+        datosVenta["total_a_pagar"] = state.detalleProductos.reduce((accumulator, producto) => accumulator + producto.total, 0)
+
+
+        setState(prevState => {
+            return { ...prevState, datosVenta }
+        })
+        //state.datosVenta.monto_neto = (state.datosVenta.monto_total/1.19).toFixed(2)
+        //state.datosVenta.monto_iva = state.datosVenta.monto_neto*(19/100).toFixed(2)
+        //state.detalleProductos===null? 0 : state.datosVenta.monto_total = state.detalleProductos.reduce((accumulator, producto) => accumulator + producto.total, 0 )
+
+
+    }
 
     const getInformacion = (e) => {
         let data = {
@@ -58,6 +75,21 @@ const IngresarVenta = (props) => {
         setState(prevState => {
             return { ...prevState, detalleProductos }
         })
+
+
+
+        const { datosVenta } = state
+        datosVenta.total_a_pagar = state.detalleProductos.reduce((accumulator, producto) => accumulator + producto.total, 0)
+        datosVenta.total_neto = Math.ceil(datosVenta["total_a_pagar"] / (1.19));
+        datosVenta.iva = Math.ceil(datosVenta.total_neto * (19 / 100));
+
+
+        setState(prevState => {
+            return { ...prevState, datosVenta }
+        })
+
+
+
     }
 
     const deleteProducto = e => {
@@ -77,16 +109,16 @@ const IngresarVenta = (props) => {
             return { ...prevState, detalleProductos }
         })
     }
-/* 
-    const getDatosVenta = e => {
-        const datosVenta = state.datosVenta;
-        datosVenta.monto_total = e.target.value
-       
-        setState(prevState => {
-            return { ...prevState, datosVenta }
-        })
-    }
- */
+    /* 
+        const getDatosVenta = e => {
+            const datosVenta = state.datosVenta;
+            datosVenta.monto_total = e.target.value
+           
+            setState(prevState => {
+                return { ...prevState, datosVenta }
+            })
+        }
+     */
 
     return (
         <>
@@ -173,7 +205,7 @@ const IngresarVenta = (props) => {
                                                         <tr>
                                                             <th scope="row">{index + 1}</th>
                                                             <td className="text-center">{producto.descripcion}</td>
-                                                    <td><input type="text" className="border-0 text-center" onChange={getDatosProductos} id={index} value={state.detalleProductos.length>0?state.detalleProductos.cantidad:""}/></td>
+                                                            <td><input type="text" className="border-0 text-center" onChange={getDatosProductos} id={index} value={state.detalleProductos.length > 0 ? state.detalleProductos.cantidad : ""} /></td>
                                                             <td className="text-center">{producto.precio_venta_unitario}</td>
                                                             <td className="text-center" >{producto.total}</td>
                                                             <td className="text-center"> <button type="button" rel="tooltip" title="" className="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Eliminar?" onClick={deleteProducto} id={index}>
@@ -198,15 +230,15 @@ const IngresarVenta = (props) => {
                                     <thead>
                                         <tr className=" table-hover">
                                             <th scope="col">Total Neto</th>
-                                            <th scope="col"><input type="text" aria-label="First name" className="form-control" placeholder="Monto Neto"  value={state.datosVenta.monto_neto = (state.datosVenta.monto_total/1.19).toFixed(2)} /></th>
+                                            <th scope="col"><input type="text" name="total_neto" aria-label="First name" className="form-control" placeholder="Monto Neto" onChange={getDatosVentas} value={state.datosVenta? state.datosVenta.total_neto:""} /></th>
                                         </tr>
                                         <tr className=" table-hover">
                                             <th scope="col">IVA 19%</th>
-                                            <th scope="col"><input type="text" aria-label="First name" className="form-control" placeholder="IVA"  value={state.datosVenta.monto_iva = state.datosVenta.monto_neto*(19/100).toFixed(2)} /></th>
+                                            <th scope="col"><input type="text" name="iva" aria-label="First name" className="form-control" placeholder="IVA" onChange={getDatosVentas} value={state.datosVenta?state.datosVenta.iva:""} /></th>
                                         </tr>
                                         <tr className=" table-hover">
                                             <th scope="col">Total a pagar</th>
-                                            <th scope="col"><input type="text" aria-label="First name" className="form-control" placeholder="Monto a Pagar" /*  onChange={getDatosVenta} */ value={state.detalleProductos===null? 0 : state.datosVenta.monto_total = state.detalleProductos.reduce((accumulator, producto) => accumulator + producto.total, 0 )} /> </th>
+                                            <th scope="col"><input type="text" name="total_a_pagar" aria-label="First name" className="form-control" placeholder="Monto a Pagar" onChange={getDatosVentas} value={state.datosVenta?state.datosVenta.total_a_pagar:""} /> </th>
                                         </tr>
                                     </thead>
                                 </table>
